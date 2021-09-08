3d7c8a09c812b52f4f6824fc1c252a1730547481
soloshand <sfields0879@gmail.com>
2021-09-08 09:59:27 -0400

    Update firebase.js

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,12 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-/**************** Firebase Configuration ********************
- * Developer: Stephen Fields
- * Creation Date: 9/27/2019
- * Modified Date: 9/27/2019
- * Comments: Configuration for Firebase database
-*************************************************************/
 const fireConfig = {
   //firebase_connection_configuration
 }
