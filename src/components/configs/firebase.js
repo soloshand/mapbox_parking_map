c4835019ad144c668ac53a792d292156fe519f6f
soloshand <sfields0879@gmail.com>
2021-09-08 09:58:53 -0400

    Update firebase.js

--- src/components/configs/firebase.js
+++ src/components/configs/firebase.js
@@ -1,13 +1,6 @@
 
 import Firebase from 'firebase';
 
-/**************** Firebase Configuration ********************
- * Developer: Stephen Fields
- * Creation Date: 9/27/2019
- * Modified Date: 9/27/2019
- * Comments: Configuration for Firebase database
-*************************************************************/
-
 const fireConfig = {
     apiKey: process.env.PARKING_APP_API_KEY,
     authDomain: process.env.PARKING_APP_AUTH_DOMAIN,
